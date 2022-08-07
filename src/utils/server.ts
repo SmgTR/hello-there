87a0ba9d05387408213e5d9b124062645187542e
SmgTr <59412217+SmgTR@users.noreply.github.com>
2022-08-07 09:26:06 +0200

    passport strategy

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,14 +1,21 @@
 import express from 'express';
-
 import bodyParser from 'body-parser';
-import { usersRouter } from '@/routes';
+import passport from 'passport';
+
+import { usersRouter, swRouter } from '@/routes';
+import { strategy } from '@/auth/passport';
 
 function createServer() {
   const app = express();
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
 
+  app.use(passport.initialize());
+
+  passport.use(strategy);
+
   app.use('/api/v1', usersRouter);
+  app.use('/api/v1/starwars', swRouter);
 
   return app;
 }
